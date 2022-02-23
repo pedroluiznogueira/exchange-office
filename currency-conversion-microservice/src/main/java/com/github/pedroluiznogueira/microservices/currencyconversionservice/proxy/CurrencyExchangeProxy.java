c6f46d8b0c3b4fce317398e83cffd7c496eea935
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-23 02:17:51 -0300

    docs v.1.1

--- currency-conversion-microservice/src/main/java/com/github/pedroluiznogueira/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-microservice/src/main/java/com/github/pedroluiznogueira/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,8 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//CHANGE-KUBERNETES
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
